b12e419e6f5d0955a610443b7adc5aeeb2f25ddb
Ian Emnace <igemnace@gmail.com>
2022-12-03 16:20:25 +0800

    Solve Day 3: Rucksack Reorganization - Part 2
    As always, the plan of attack is commented in the code.

--- src/day-03/day-03.js
+++ src/day-03/day-03.js
@@ -36,9 +36,33 @@ const { promises: fs } = require('fs');
  * all consecutive in ASCII (and therefore, also in Unicode), so I can just
  * compute priority from that with a bit of math. This way I don't have to
  * statically set up each letter...
+ *
+ * Part 2:
+ *
+ * There's some reorganization that I'll have to do here. The lines in the input
+ * are now grouped into three, and instead of checking for common letters
+ * between halves, I now have to find common letters across the three grouped
+ * lines.
+ *
+ * Some amendments I'm thinking of:
+ * 1a. No longer needed to split into two. Separate the splitting out into its
+ * own function.
+ * 1b. Now needed to group lines into groups of 3s. Separate that out also into
+ * its own function.
+ * 2a. Overload the comparator to handle variadic arguments. Algo I'm thinking
+ * of here is:
+ *   0. Instead of a first string and a second string, think in terms of
+ *   "previous string" and "current string".
+ *   1. Build an existence map for the previous string.
+ *   2. Build an existence map for the current string as well: run through the
+ *   current string, while checking against the previous existence map: if
+ *   existing in both, add to the current existence map.
+ *   3. Move to the next string. The current existence map will now be its
+ *   previous existence map. This should be able to handle finding common
+ *   letters for any number of strings.
  */
 
-function parseRucksackCompartments(string) {
+function parseLines(string) {
   /**
    * Remove terminating newline if existing, so it's easy to parse lines with
    * String#split.
@@ -51,8 +75,11 @@ function parseRucksackCompartments(string) {
     ? string.slice(0, string.length - 1)
     : string;
 
-  return separatedString.split('\n')
-    .map(line => ([
+  return separatedString.split('\n');
+}
+
+function parseRucksackCompartments(lines) {
+  return lines.map(line => ([
       // split into halves. Array#slice uses the typical [a, b) interval in
       // computing so the bins work out quite nicely
       line.slice(0, line.length / 2),
@@ -60,19 +87,58 @@ function parseRucksackCompartments(string) {
     ]));
 }
 
-function findCommon(a, b) {
-  // O(m): build the existence map
-  const letterMap = new Map();
-  for (const letter of a) {
-    letterMap.set(letter, true);
+function parseRucksackGroups(lines, numPerGroup) {
+  const groups = [];
+  let currentGroup = [];
+
+  // group into numPerGroup
+  for (const line of lines) {
+    currentGroup.push(line);
+
+    // flush to groups once group size is hit
+    if (currentGroup.length === numPerGroup) {
+      groups.push(currentGroup);
+      currentGroup = [];
+    }
   }
 
-  // O(n): check the second string against the map
-  for (const letter of b) {
-    if (letterMap.has(letter)) return letter;
+  // check for leftovers after the loop
+  // these are the remainder if lines is not divisible by numPerGroup
+  if (currentGroup.length) groups.push(currentGroup);
+
+  return groups;
+}
+
+/**
+ * If n is the length of strings and m is the number of strings, time complexity
+ * here should be O(n * m). We win against the naive case of nested iterations
+ * without building maps, which is O(n ^ m).
+ *
+ * Notice, this is a generalized case of our previous findCommon, where m was 2:
+ * building the map gets you O(n * 2), which is linear time, whereas the naive
+ * approach gets you O(n ^ 2), which is quadratic time.
+ */
+function findCommon(...strings) {
+  // start previousMap at null, so we have a well-defined initial condition
+  let previousMap = null;
+  let currentMap = new Map();
+
+  for (const string of strings) {
+    for (const letter of string) {
+      // if this is our first string, just initialize the map
+      if (previousMap === null) currentMap.set(letter, true);
+
+      // on all other iterations, run through while checking against previousMap
+      else if (previousMap.has(letter)) currentMap.set(letter, true);
+    }
+
+    // set up next iteration
+    previousMap = currentMap;
+    currentMap = new Map();
   }
 
-  // total algo is O(m + n), which is just O(n) -- linear time
+  // after the loop, previousMap will contain all the common letters
+  return [...previousMap.keys()];
 }
 
 /**
@@ -126,9 +192,12 @@ function getPriority(letter) {
 async function main() {
   // read list from file
   const input = await fs.readFile('./data', { encoding: 'utf8' });
-  const rucksacks = parseRucksackCompartments(input);
-  const commonLetters = rucksacks.map(([a, b]) => findCommon(a, b));
-  const priorities = commonLetters.map(getPriority);
+  const lines = parseLines(input);
+  // group into 3
+  const groups = parseRucksackGroups(lines, 3);
+  const commonLetters = groups.map(([a, b, c]) => findCommon(a, b, c));
+  // we expect just 1 common letter
+  const priorities = commonLetters.map(([letter]) => getPriority(letter));
 
   // print result
   console.log(priorities.reduce((acc, p) => acc + p, 0));
